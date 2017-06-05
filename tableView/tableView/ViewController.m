525beb4d23a938172c5471c11c1dbdd5d9ac5a2a
钱超 <qianchao@qianchaodeMacBook-Pro.local>
2017-06-06 02:43:59 +0800

    追加图标 置顶功能以及江全部cell删除后，出现提示页面

--- tableView/tableView/ViewController.m
+++ tableView/tableView/ViewController.m
@@ -12,6 +12,10 @@
 
 @property (strong,nonatomic) NSMutableArray *items;
 
+@property (strong,nonatomic) NSMutableArray *detailItems;
+
+@property (strong,nonatomic) NSMutableArray *imageItems;
+
 @property (strong,nonatomic) UITableView *tableView;
 
 @end
@@ -21,28 +25,56 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.items =  [NSMutableArray arrayWithObjects:@"东邪",@"西毒",@"南帝",@"北丐",nil];
+    self.items =  [NSMutableArray arrayWithObjects:@"我是苹果",@"我是葡萄",@"我是柠檬",@"我是芒果",@"我是橙子",@"我是梨子",@"我是菠萝",@"我是石榴",@"我是草莓",@"我是西瓜",nil];
+    
+    self.detailItems = [NSMutableArray arrayWithObjects:@"我是苹果我是苹果我是苹果我是苹果我是苹果",@"我是葡萄我是葡萄我是葡萄我是葡萄我是葡萄",@"我是柠檬我是柠檬我是柠檬我是柠檬我是柠檬",@"我是芒果我是芒果我是芒果我是芒果我是芒果",@"我是橙子我是橙子我是橙子我是橙子我是橙子",@"我是梨子我是梨子我是梨子我是梨子我是梨子",@"我是菠萝我是菠萝我是菠萝我是菠萝我是菠萝",@"我是石榴我是石榴我是石榴我是石榴我是石榴",@"我是草莓我是草莓我是草莓我是草莓我是草莓",@"我是西瓜我是西瓜我是西瓜我是西瓜我是西瓜",nil];
+    
+    self.imageItems = [NSMutableArray arrayWithObjects:@"apple",@"grape",@"lemon",@"mango",@"orange",@"pear",@"pineapple",@"pomegrante",@"strawberry",@"watermelon",nil];
     
     // Do any additional setup after loading the view, typically from a nib.
     
-    UITableView *_tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 60, self.view.bounds.size.width, self.view.bounds.size.height) style:UITableViewStylePlain];
+    //初始化UITableView
+    UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 60, self.view.bounds.size.width, 1500) style:UITableViewStylePlain];
  
-    _tableView.rowHeight = 60;
+    tableView.rowHeight = 60;
     
-    _tableView.delegate = self;
+    tableView.delegate = self;
     
-    _tableView.dataSource = self;
+    tableView.dataSource = self;
     
-    [self.view addSubview:_tableView];
+    tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    
+    self.title = @"水果天堂";
+    
+    self.view.backgroundColor = [UIColor colorWithRed:1.00 green:0.91 blue:0.20 alpha:1.00];
+    
+    [self.view addSubview:tableView];
     
     
 }
 
-
+//定义行数
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return _items.count;
 }
 
+
+-(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row==self.items.count-1) {
+        cell.separatorInset = UIEdgeInsetsMake(0, 320 , 0, 0);
+    }
+    
+    if ([cell respondsToSelector:@selector(setSeparatorInset:)]) {
+        [cell setSeparatorInset:UIEdgeInsetsZero];
+    }
+    
+    if ([cell respondsToSelector:@selector(setLayoutMargins:)]) {
+        [cell setLayoutMargins:UIEdgeInsetsZero];
+    }
+}
+
+//数据置入以及样式设置
 - (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     
@@ -54,9 +86,28 @@
     
     cell.textLabel.text = [self.items objectAtIndex:indexPath.row];
     
-    cell.detailTextLabel.text = @"详细信息";
+    cell.detailTextLabel.text = [self.detailItems objectAtIndex:indexPath.row];
     
-    cell.contentView.backgroundColor = [UIColor colorWithRed:0.94 green:0.94 blue:0.96 alpha:1.00];
+//    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"apple.jpg"]];
+    
+    NSString *imageName = [self.imageItems objectAtIndex:indexPath.row];
+    
+    cell.imageView.image = [UIImage imageNamed:imageName];
+    
+    //修改图片大小
+    CGSize itemSize = CGSizeMake(40, 40);
+    
+    UIGraphicsBeginImageContextWithOptions(itemSize, NO, UIScreen.mainScreen.scale);
+    
+    CGRect imageRect = CGRectMake(0.0, 0.0, itemSize.width, itemSize.height);
+    
+    [cell.imageView.image drawInRect:imageRect];
+    
+    cell.imageView.image = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    
+    cell.contentView.backgroundColor = [UIColor colorWithRed:0.98 green:0.98 blue:0.98 alpha:1.00];
     
     return cell;
 }
@@ -67,6 +118,7 @@
     return YES;
 }
 
+//设置cell左划菜单
 - (NSArray<UITableViewRowAction *> *)tableView:(UITableView *)tableView editActionsForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     //删除
@@ -74,18 +126,42 @@
         
         NSLog(@"点击了删除");
     
+        //删除操作的实现
         [self.items removeObjectAtIndex:indexPath.row];
 
-        NSMutableArray * indexPathArray = [NSMutableArray arrayWithObject:indexPath];
+        NSMutableArray *indexPathArray = [NSMutableArray arrayWithObject:indexPath];
         
-        [tableView deleteRowsAtIndexPaths:indexPathArray withRowAnimation:UITableViewRowAnimationFade];
+        [tableView deleteRowsAtIndexPaths:indexPathArray withRowAnimation:UITableViewRowAnimationNone];
         
+        if (self.items.count == 0) {
+            UILabel *noDataLabel = [[UILabel alloc] initWithFrame:CGRectMake(103, 200, 114, 30)];
+            
+            noDataLabel.text = @"暂无数据 请稍后尝试";
+            
+            noDataLabel.textColor = [UIColor grayColor];
+            
+            [noDataLabel setFont:[UIFont systemFontOfSize:12]];
+            
+            [self.view addSubview:noDataLabel];
+        }
     }];
+    
     deleteRowAction.backgroundColor = [UIColor greenColor];
+    
     //置顶
     UITableViewRowAction *topRowAction = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleNormal title:@"置顶" handler:^(UITableViewRowAction * _Nonnull action, NSIndexPath * _Nonnull indexPath) {
         
         NSLog(@"点击了置顶");
+        
+        //置顶功能的实现
+        //1.更新数据
+        [self.items exchangeObjectAtIndex:indexPath.row withObjectAtIndex:0];
+        
+        //2.更新UI
+        NSIndexPath *firstIndexPath =[NSIndexPath indexPathForRow:0 inSection:indexPath.section];
+        [tableView moveRowAtIndexPath:indexPath toIndexPath:firstIndexPath];
+        
+        [tableView setEditing:NO animated:YES];
     }];
     
     //标记为已读
@@ -112,6 +188,8 @@
 //    }
 }
 
+
+//点击cell时的操作
 - (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     
     
@@ -122,6 +200,30 @@
     cell.textLabel.text = @"我是新标题";
     
     NSLog(@"第%ld行的内容是%@",indexPath.row,cell.detailTextLabel.text);
+}
+
+
+- (void)viewWillAppear:(BOOL)animated{
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationWillEnterForeground)name:UIApplicationWillEnterForegroundNotification object:nil];
+    
+}
+
+- (void)applicationWillEnterForeground{
+    
+    NSArray *indexPaths = @[
+                            [NSIndexPath indexPathForRow:0 inSection:0],
+                            [NSIndexPath indexPathForRow:1 inSection:0]
+                            ];
+    [self.tableView deleteRowsAtIndexPaths:indexPaths withRowAnimation:UITableViewRowAnimationMiddle];
+    
+    NSLog(@"回到前台");
+}
+
+- (void)viewDidDisappear:(BOOL)animated{
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    
 }
 
 - (void)didReceiveMemoryWarning {
